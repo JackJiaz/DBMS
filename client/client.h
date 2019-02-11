ebf54cb0a532a4d17380848ae1cf13d9c3952d6a
JackJiaz <943678436@qq.com>
2019-02-11 15:57:53 +0800

    something new

---
+++ client/client.h
@@ -0,0 +1,34 @@
+/*************************************************************************
+	> File Name: client.h
+	> Author: gpx
+	> Mail: 1457495424@qq.com
+	> Created Time: 2018年11月25日 星期日 09时25分06秒
+ ************************************************************************/
+
+#ifndef _CLIENT_H
+#define _CLIENT_H
+
+#include <pthread.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/file.h>
+#include <signal.h>
+#include <time.h>
+#include <stdarg.h>
+
+
+#endif
